937005ecc1120ef640f925b944b1218e4fb67b38
Khaled <khaledbm12@gmail.com>
2023-01-19 11:28:30 +0100

    Added game function and score counter

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const rps = ["Rock", "Paper", "Scissors"]
-
+let playerScore = 0, computerScore = 0
 
 function getComputerChoice() {
     let rand = (min, max) => {return Math.floor(Math.random() * (max - min)) + min;};
@@ -8,22 +8,26 @@ function getComputerChoice() {
 }   
 
 function outcome(playerChoice, computerSelection) {
-    playerChoice = playerChoice.toUpperCase();
-    computerSelection = getComputerChoice().toUpperCase();
-    if ((computerSelection == "ROCK") && (playerChoice == "SCISSORS") || (computerSelection == "SCISSORS") && (playerChoice == "PAPPER") || (computerSelection == "PAPPER") && (playerChoice == "ROCK")) {
+    if (((computerSelection.toUpperCase() == "ROCK") && (playerChoice.toUpperCase() == "SCISSORS")) || ((computerSelection.toUpperCase() == "SCISSORS") && (playerChoice.toUpperCase() == "PAPPER")) || ((computerSelection.toUpperCase() == "PAPPER") && (playerChoice.toUpperCase() == "ROCK"))) {
+        computerScore +=1
         return "You Lose! " + computerSelection + " beats " + playerChoice + "."
     }
-    else if (playerChoice == computerSelection) {
+    else if (playerChoice === computerSelection) {
         return "It's a Draw! Both played " + playerChoice +  "."
         }
     else {
+        playerChoice +=1
         return "You Win! " + playerChoice +  " beats " + computerSelection + "."
     }
 }
 
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerChoice = rps[prompt("Choose : 1. Rock, 2. Paper, 3. Scissors :", 0)-1]
+        console.log(outcome(playerChoice,  getComputerChoice()));
+    }
+    console.log("Score : Player : " + playerScore + " Computer : " + computerScore);
+}
 
+game()
 
-
-console.log(getComputerChoice());
-
-console.log(outcome("Scissors", getComputerChoice()));
