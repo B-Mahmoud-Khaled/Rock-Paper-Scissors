899a8810e98bb4dd28079b4279736eb116fddda4
Khaled <khaledbm12@gmail.com>
2023-01-19 10:34:41 +0100

    Added outcome function to compare player and pc choice

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-function getComputerChoice() {
-    const rps = ["Rock", "Paper", "Scissors"]
-    let x = (min, max) => {return Math.floor(Math.random() * (max - min)) + min;};
+const rps = ["Rock", "Paper", "Scissors"]
 
-    return rps[x(0,3)]
+
+function getComputerChoice() {
+    let rand = (min, max) => {return Math.floor(Math.random() * (max - min)) + min;};
+
+    return rps[rand(0,3)] 
 }   
+
+function outcome(playerChoice, computerSelection) {
+    playerChoice = playerChoice.toUpperCase();
+    computerSelection = getComputerChoice().toUpperCase();
+    if ((computerSelection == "ROCK") && (playerChoice == "SCISSORS") || (computerSelection == "SCISSORS") && (playerChoice == "PAPPER") || (computerSelection == "PAPPER") && (playerChoice == "ROCK")) {
+        return "You Lose! " + computerSelection + " beats " + playerChoice + "."
+    }
+    else if (playerChoice == computerSelection) {
+        return "It's a Draw! Both played " + playerChoice +  "."
+        }
+    else {
+        return "You Win! " + playerChoice +  " beats " + computerSelection + "."
+    }
+}
+
+
+
 
 console.log(getComputerChoice());
 
+console.log(outcome("Scissors", getComputerChoice()));
