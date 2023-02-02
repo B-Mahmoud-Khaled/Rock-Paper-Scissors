a2f5e634f4b27d093d64710fda81e147fc8d33c6
Khaled <khaledbm12@gmail.com>
2023-02-02 11:13:54 +0100

    Cleaned up the functions and fixed the event listeners

--- index.js
+++ index.js
@@ -11,26 +11,38 @@ const div1 = document.querySelector('.rpsContainer');
 const div2 = document.querySelector('.display');
 const div3 = document.createElement('div');
 const score = document.createElement('div')
+const outcome = document.createElement('div')
+
 
 btn1.classList.toggle("rock");
 btn2.classList.toggle("paper");
 btn3.classList.toggle("scissors");
 div3.classList.toggle("outcome");
 score.classList.toggle("score");
+outcome.classList.toggle("outcome")
 
 div1.appendChild(btn1);
 div1.appendChild(btn2);
 div1.appendChild(btn3);
 div2.appendChild(score);
+div2.appendChild(outcome);
 
 btn1.textContent = "Rock";
 btn2.textContent = "Paper";
 btn3.textContent = "Scissors";
-score.textContent = scoreCount();
 
-btn1.addEventListener('click', () => playerChoice = btn1.textContent.toUpperCase())
-btn2.addEventListener('click', () => playerChoice = btn2.textContent.toUpperCase())
-btn3.addEventListener('click', () => playerChoice = btn3.textContent.toUpperCase())
+btn1.addEventListener('click', () => {
+    playerChoice = btn1.textContent;
+    game();
+});
+btn2.addEventListener('click', () => {
+    playerChoice = btn2.textContent;
+    game();
+})
+btn3.addEventListener('click', () => {
+    playerChoice = btn3.textContent;
+    game();
+})
 
 function getComputerChoice() {
     let rand = (min, max) => { return Math.floor(Math.random() * (max - min)) + min; };
@@ -38,10 +50,10 @@ function getComputerChoice() {
     return rps[rand(0, 3)]
 }
 
-function outcome(playerChoice, computerSelection) {
-    if (((computerSelection.toUpperCase() == "ROCK") && (playerChoice.toUpperCase() == "SCISSORS")) || 
-        ((computerSelection.toUpperCase() == "SCISSORS") && (playerChoice.toUpperCase() == "PAPER")) || 
-        ((computerSelection.toUpperCase() == "PAPER") && (playerChoice.toUpperCase() == "ROCK"))) {
+function getOutcome() {
+    if (((computerSelection == "Rock") && (playerChoice == "Scissors")) ||
+        ((computerSelection == "Scissors") && (playerChoice == "Paper")) ||
+        ((computerSelection == "Paper") && (playerChoice == "Rock"))) {
         computerScore++;
         return "You Lose! " + computerSelection + " beats " + playerChoice + "."
     }
@@ -54,15 +66,14 @@ function outcome(playerChoice, computerSelection) {
     }
 }
 
-function scoreCount() {
+function getScoreCount() {
     return "Score : Player : " + playerScore + " Computer : " + computerScore;
 }
 
 function game() {
-        pick = (prompt("Choose : 1. Rock, 2. Paper, 3. Scissors :", 0) - 1)
-        playerChoice = rps[pick]
-        console.log(outcome(playerChoice, getComputerChoice()));
-    console.log(scoreCount());
+    computerSelection = getComputerChoice();
+    outcome.textContent = getOutcome();
+    score.textContent = getScoreCount();
 }
 
 
